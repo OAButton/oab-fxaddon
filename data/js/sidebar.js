f3c71710e2900aa0644cac2e7846c371a1c545ed
Harry Rickards <rickards@mit.edu>
2014-10-12 10:50:48 -0400

    Add success/failure sharing screens

--- data/js/sidebar.js
+++ data/js/sidebar.js
@@ -1,28 +1,42 @@
 window.onload = function() {
-	document.getElementById("loading").style.display = 'block';
-	document.getElementById("loaded").style.display = 'none';
+	showLoading();
 
 	document.getElementById("logout_button").onclick = function() {
 		addon.port.emit("logout");
 		return false;
 	}
 
+	document.getElementById("done_button").onclick = function() {
+		showLoaded();
+	}
+
 	addon.port.emit("page_loaded");
 }
 
 addon.port.on("info_obtained", function(success, html, share_url) {
-	document.getElementById("loading").style.display = 'none';
-	document.getElementById("loaded").style.display = 'block';
+	showLoaded();
 
 	document.getElementById("response").innerHTML = html;
 
 	var twitter_link = "https://twitter.com/home?status=See%20what%20I%E2%80%99d%20do%20with%20access%20to%20this%20research%20paper%20at%20" + encodeURI(share_url) + "%20via%20@oa_button."
+	$(".twitter").off("click");
+	$(".twitter").on("click", function() {
+		window.open(twitter_link,'_blank');
+	});
 	document.getElementById("twitter").setAttribute("href", twitter_link);
 
 	var google_link = "https://plus.google.com/share?url=" + encodeURI(share_url);
+	$(".google").off("click");
+	$(".google").on("click", function() {
+		window.open(google_link,'_blank');
+	});
 	document.getElementById("google").setAttribute("href", google_link);
 
 	var facebook_link = "https://www.facebook.com/sharer/sharer.php?u=" + encodeURI(share_url);
+	$(".facebook").off("click");
+	$(".facebook").on("click", function() {
+		window.open(facebook_link,'_blank');
+	});
 	document.getElementById("facebook").setAttribute("href", facebook_link);
 
 	document.getElementById("access_button").onclick = function() {
@@ -41,6 +55,14 @@ addon.port.on("info_obtained", function(success, html, share_url) {
 	}
 });
 
+addon.port.on("success", function() {
+	showSuccess();
+});
+
+addon.port.on("failure", function() {
+	showFailure();
+});
+
 addon.port.on("check_email", function() {
 	// TODO: Why is this always null
 	var all_emails = document.documentElement.innerHTML.toLowerCase().match(/([a-z0-9_\.\-\+]+@[a-z0-9_\-]+(\.[a-z0-9_\-]+)+)/g);
@@ -55,3 +77,35 @@ addon.port.on("check_email", function() {
 
 	addon.port.emit("got_emails", emails);
 });
+
+function showLoading() {
+	$("#loading").show();
+	$("#loaded").hide();
+	$(".success").hide();
+	$(".failure").hide();
+	$(".social_share").hide();
+}
+
+function showLoaded() {
+	$("#loading").hide();
+	$("#loaded").show();
+	$(".success").hide();
+	$(".failure").hide();
+	$(".social_share").hide();
+}
+
+function showSuccess() {
+	$("#loading").hide();
+	$("#loaded").hide();
+	$(".success").show();
+	$(".failure").hide();
+	$(".social_share").show();
+}
+
+function showFailure() {
+	$("#loading").hide();
+	$("#loaded").hide();
+	$(".success").hide();
+	$(".failure").show();
+	$(".social_share").show();
+}
