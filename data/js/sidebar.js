576451ec9577a39c4a7eb56412f6b535db2f8648
Harry Rickards <rickards@mit.edu>
2014-10-12 11:36:56 -0400

    Get email regex properly working now

--- data/js/sidebar.js
+++ data/js/sidebar.js
@@ -63,21 +63,6 @@ addon.port.on("failure", function() {
 	showFailure();
 });
 
-addon.port.on("check_email", function() {
-	// TODO: Why is this always null
-	var all_emails = document.documentElement.innerHTML.toLowerCase().match(/([a-z0-9_\.\-\+]+@[a-z0-9_\-]+(\.[a-z0-9_\-]+)+)/g);
-	var emails = [];
-
-	for (var i=0; i<all_emails.length; i++) {
-		var email = all_emails[i];
-		if (!((email.indexOf("@elsevier.com") > -1) || (email.indexOf("@nature.com") > -1) || (email.indexOf("@sciencemag.com") > -1) || (email.indexOf("@springer.com") > -1))) {
-			emails.push(email);
-		}
-	}
-
-	addon.port.emit("got_emails", emails);
-});
-
 function showLoading() {
 	$("#loading").show();
 	$("#loaded").hide();
