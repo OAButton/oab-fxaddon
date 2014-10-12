7be156d5dfaf9a97979616c5dfb03ab56a1ccd0b
Harry Rickards <rickards@mit.edu>
2014-10-12 09:00:29 -0400

    Get working with backend

--- data/js/sidebar.js
+++ data/js/sidebar.js
@@ -2,40 +2,39 @@ window.onload = function() {
 	document.getElementById("loading").style.display = 'block';
 	document.getElementById("loaded").style.display = 'none';
 
-	document.getElementById("report_button").onclick = function() {
-		addon.port.emit("report_paywall");
-	}
-
 	document.getElementById("logout_button").onclick = function() {
 		addon.port.emit("logout");
-	}
-
-	document.getElementById("report_button").onclick = function() {
-		var story = document.getElementById("story").value;
-		addon.port.emit("submit_blocked", story, true);
+		return false;
 	}
 }
 
-addon.port.on("info_obtained", function(links, related_papers, author_email) {
+addon.port.on("info_obtained", function(success, html, share_url) {
 	document.getElementById("loading").style.display = 'none';
 	document.getElementById("loaded").style.display = 'block';
 
-	var links_html = "";
-	for(var i=0; i<links.length; i++) {
-		links_html += "<li>" + "<a href='" + links[i].url + "'>" + links[i].source + "</a>" + "</li>";
+	document.getElementById("response").innerHTML = html;
+
+	var twitter_link = "https://twitter.com/home?status=See%20what%20I%E2%80%99d%20do%20with%20access%20to%20this%20research%20paper%20at%20" + encodeURI(share_url) + "%20via%20@oa_button."
+	document.getElementById("twitter").setAttribute("href", twitter_link);
+
+	var google_link = "https://plus.google.com/share?url=" + encodeURI(share_url);
+	document.getElementById("google").setAttribute("href", google_link);
+
+	var facebook_link = "https://www.facebook.com/sharer/sharer.php?u=" + encodeURI(share_url);
+	document.getElementById("facebook").setAttribute("href", facebook_link);
+
+	document.getElementById("access_button").onclick = function() {
+		var story = document.getElementById("story").value;	
+		var title;
+		if (document.getElementById("title") == null) {
+			title = null;
+		} else {
+			title = document.getElementById("title").value;
+		}
+		addon.port.emit("got_access", story, title);
 	}
-	document.getElementById("links").innerHTML = links_html;
 
-	var related_papers_html = "";
-	for(var i=0; i<related_papers.length; i++) {
-		related_papers_html += "<li>" + "<a href='" + related_papers[i].url + "'>" + related_papers[i].title + "</a>" + "</li>";
-	}
-	document.getElementById("related_papers").innerHTML = related_papers_html;
-
-	document.getElementById("author_button").onclick = function() {
-		var story = document.getElementById("story").value;
-		addon.port.emit("submit_blocked", story, false);
-
-		window.location.href = "mailto:" + author_email + "?body=" + encodeURIComponent(story);
+	document.getElementById("no_access_button").onclick = function() {
+		addon.port.emit("didnt_get_access");
 	}
 });
